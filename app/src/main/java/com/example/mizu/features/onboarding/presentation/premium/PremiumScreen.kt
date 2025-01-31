a85aecb19305a245632e05599d31f25b9a0381cd
Hitoli <kohihitesh@gmail.com>
2025-01-31 23:21:39 +0530

    Payment Screen UI

--- app/src/main/java/com/example/mizu/features/onboarding/presentation/premium/PremiumScreen.kt
+++ app/src/main/java/com/example/mizu/features/onboarding/presentation/premium/PremiumScreen.kt
@@ -1,6 +1,7 @@
 package com.example.mizu.features.onboarding.presentation.premium
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,9 +9,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBackIosNew
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -18,6 +21,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -32,7 +37,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.mizu.ui.theme.backgroundColor2
 import com.example.mizu.ui.theme.fontFamily
+import com.example.mizu.ui.theme.fontFamilyLight
 import com.example.mizu.ui.theme.mizuBlack
+import com.example.mizu.ui.theme.onboardingBoxColor
+import com.example.mizu.ui.theme.waterColor
 import com.example.mizu.ui.theme.waterColorMeter
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -55,60 +63,214 @@ fun PremiumScreen(modifier: Modifier = Modifier) {
                 )
             }, navigationIcon = {
                 Icon(imageVector = Icons.Rounded.ArrowBackIosNew, contentDescription = "Arrow")
-            })
+            }, colors = TopAppBarDefaults.topAppBarColors(waterColorMeter.copy(alpha = 0.1f)))
         }
     ) {
         val pad = it
         Column(
-            modifier = modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.SpaceEvenly,
+            modifier = modifier
+                .fillMaxSize()
+                .padding(
+                    start = 16.dp,
+                    end = 16.dp,
+                    top = pad.calculateTopPadding(),
+                    bottom = pad.calculateBottomPadding()
+                ),
+            verticalArrangement = Arrangement.SpaceBetween,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Row(
-                modifier = Modifier.wrapContentSize(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Column(
-                    modifier = Modifier
-                        .weight(1f)
-                        .background(Color.White)
-                        .wrapContentSize(),
-                    verticalArrangement = Arrangement.SpaceEvenly,
-                    horizontalAlignment = Alignment.Start
+
+            Column(modifier = Modifier
+                .wrapContentSize()
                 ) {
-                    Text(
-                        text = "Monthly",
-                        style = TextStyle(
-                            fontSize = 18.sp,
-                            fontFamily = fontFamily,
-                            fontWeight = FontWeight(400),
-                            color = mizuBlack,
-                            textAlign = TextAlign.Center,
-                        ), modifier = Modifier.fillMaxWidth()
-                    )
+                Row(
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .padding(top = 20.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .weight(1f)
+                            .background(Color.White, shape = RoundedCornerShape(12.dp))
+                            .wrapContentSize()
+                            .padding(20.dp),
+                        verticalArrangement = Arrangement.SpaceEvenly,
+                        horizontalAlignment = Alignment.Start
+                    ) {
+                        Text(
+                            text = "Monthly",
+                            style = TextStyle(
+                                fontSize = 12.sp,
+                                fontFamily = fontFamily,
+                                fontWeight = FontWeight(400),
+                                color = mizuBlack,
+                            ), modifier = Modifier.fillMaxWidth()
+                        )
+                        Spacer(modifier = Modifier.height(10.dp))
+                        Text(
+                            text = "$9.99",
+                            style = TextStyle(
+                                fontSize = 20.sp,
+                                fontFamily = fontFamily,
+                                fontWeight = FontWeight(600),
+                                color = mizuBlack,
+                            ), modifier = Modifier.fillMaxWidth()
+                        )
+                        Spacer(modifier = Modifier.height(50.dp))
+                        Text(
+                            text = "Billed Monthly",
+                            style = TextStyle(
+                                fontSize = 12.sp,
+                                fontFamily = fontFamily,
+                                fontWeight = FontWeight(400),
+                                color = mizuBlack,
+                            ), modifier = Modifier.fillMaxWidth()
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(25.dp))
+                    Column(
+                        modifier = Modifier
+                            .weight(1f)
+                            .background(Color.White, shape = RoundedCornerShape(12.dp))
+                            .wrapContentSize()
+                            .padding(20.dp),
+                        verticalArrangement = Arrangement.SpaceEvenly,
+                        horizontalAlignment = Alignment.Start
+                    ) {
+                        Text(
+                            text = "Lifetime",
+                            style = TextStyle(
+                                fontSize = 12.sp,
+                                fontFamily = fontFamily,
+                                fontWeight = FontWeight(400),
+                                color = mizuBlack,
+                            ), modifier = Modifier.fillMaxWidth()
+                        )
+                        Spacer(modifier = Modifier.height(10.dp))
+                        Text(
+                            text = "$12.00",
+                            style = TextStyle(
+                                fontSize = 20.sp,
+                                fontFamily = fontFamily,
+                                fontWeight = FontWeight(600),
+                                color = mizuBlack,
+                            ), modifier = Modifier.fillMaxWidth()
+                        )
+                        Spacer(modifier = Modifier.height(50.dp))
+                        Text(
+                            text = "Billed One Time",
+                            style = TextStyle(
+                                fontSize = 12.sp,
+                                fontFamily = fontFamily,
+                                fontWeight = FontWeight(400),
+                                color = mizuBlack,
+                            ), modifier = Modifier.fillMaxWidth()
+                        )
+                    }
                 }
-                Spacer(modifier = Modifier.width(25.dp))
+                
+                Spacer(modifier = Modifier.height(80.dp))
+
                 Column(
                     modifier = Modifier
-                        .weight(1f)
-                        .background(Color.White)
                         .wrapContentSize(),
-                    verticalArrangement = Arrangement.SpaceEvenly,
-                    horizontalAlignment = Alignment.Start
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
+
                     Text(
-                        text = "",
+                        text = "• Get Access to Home Screen Widget",
                         style = TextStyle(
                             fontSize = 18.sp,
                             fontFamily = fontFamily,
-                            fontWeight = FontWeight(400),
+                            fontWeight = FontWeight(600),
                             color = mizuBlack,
-                            textAlign = TextAlign.Center,
                         ), modifier = Modifier.fillMaxWidth()
                     )
+                    Spacer(modifier = Modifier.height(5.dp))
+                    Text(
+                        text = "• Set Custom Daily Reminders",
+                        style = TextStyle(
+                            fontSize = 18.sp,
+                            fontFamily = fontFamily,
+                            fontWeight = FontWeight(600),
+                            color = mizuBlack,
+                        ), modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.height(5.dp))
+
+                    Text(
+                        text = "• Advanced Tracking and Analytics",
+                        style = TextStyle(
+                            fontSize = 18.sp,
+                            fontFamily = fontFamily,
+                            fontWeight = FontWeight(600),
+                            color = mizuBlack,
+                        ), modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.height(5.dp))
+
+                    Text(
+                        text = "• Ad-Free Experience",
+                        style = TextStyle(
+                            fontSize = 18.sp,
+                            fontFamily = fontFamily,
+                            fontWeight = FontWeight(600),
+                            color = mizuBlack,
+                        ), modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.height(5.dp))
+
                 }
             }
+
+
+
+            Column(modifier= Modifier
+                .wrapContentSize()
+                ) {
+                Box(
+                    modifier = Modifier
+                        .clickable {
+//                        getNavigate()
+                        }
+                        .fillMaxWidth()
+                        .height(90.dp)
+                        .padding(horizontal = 20.dp, vertical = 20.dp)
+                        .background(waterColor, shape = RoundedCornerShape(6.dp)),
+                ) {
+                    Text(
+                        text = "Subscribe",
+                        style = TextStyle(
+                            fontSize = 16.sp,
+                            fontFamily = fontFamilyLight,
+                            fontWeight = FontWeight(400),
+                            color = mizuBlack,
+                            textAlign = TextAlign.Center,
+                        ), modifier = Modifier
+                            .fillMaxWidth()
+                            .align(Alignment.Center)
+                    )
+
+
+                }
+
+                Text(
+                    text = "Skip",
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        fontFamily = fontFamilyLight,
+                        fontWeight = FontWeight(400),
+                        color = mizuBlack,
+                        textAlign = TextAlign.Center,
+                    ), modifier = Modifier
+                        .fillMaxWidth()
+                )
+            }
+
+
         }
 
     }
